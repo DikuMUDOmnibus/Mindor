a125f85505122081ee7421b60fb59772d2e8dac9
Peter J. Schroeder <peterjschroeder@gmail.com>
2017-11-22 21:43:51 -0500

    first commit

---
+++ src/relationship.h
@@ -0,0 +1,6 @@
+#define ISTDATING(ch,vch)        (is_exact_name(ch->name, vch->pcdata->tspouse))
+#define ISDATING(ch,vch)        (is_exact_name(ch->name, vch->pcdata->spouse))
+//#define MSL MAX_STRING_LENGTH
+//#define MIL MAX_INPUT_LENGTH
+//#define stc send_to_char
+//#define ptc printf_to_char
